5b492590c1ef5088550d1189def49f40f519448e
liammcdonald97 <liammcdonald97@gmail.com>
2018-10-31 17:19:20 -0400

    updated both classes

---
+++ Class1_method_practice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_method_practice
+{
+    public class Class1
+    {                  
+          public static int TimesTwo(int number1)
+        {
+              return number1 * 2;
+        }
+
+           public static int DivideByTwo(int number1)
+        {
+            return number1 / 2;
+        }
+
+          public static int Squared(int number1)
+        {
+              return number1 * number1;
+        }       
+    }
+}
---
+++ Program_class_methods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_method_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What number do you want to multiply by 2, divide by 2, and square?");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int output1 = Class1.TimesTwo(number1);
+            Console.WriteLine(output1);
+            int output2 = Class1.DivideByTwo(number1);
+            Console.WriteLine(output2);
+            int output3 = Class1.Squared(number1);
+            Console.WriteLine(output3);
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+
+    }
+    
+}
