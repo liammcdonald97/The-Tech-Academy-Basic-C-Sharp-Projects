c09ad0982a900f971f5314c1f7b3594f72d677ef
liammcdonald97 <liammcdonald97@gmail.com>
2018-11-02 16:12:32 -0400

    update

---
+++ step_114.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            Console.WriteLine(Class1.Method1(12));
+            Console.WriteLine(Class1.Method1(4.1));
+            Console.WriteLine(Class1.Method1("5"));
+            Console.ReadLine();
+        }
+    }
+    public class Class1
+    {
+        public static int Method1(int number1)
+        {
+            return number1 * 2;
+        }
+
+        public static int Method1(double number1)
+        {
+            return Convert.ToInt32(number1) * 4;
+        }
+
+        public static int Method1(string number1)
+        {
+            return Convert.ToInt32(number1) * 5;
+        }
+    }
+}
