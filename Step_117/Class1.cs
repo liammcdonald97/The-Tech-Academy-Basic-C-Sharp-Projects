ad61d460662df32461f81fd684f9834a28350317
liammcdonald97 <liammcdonald97@gmail.com>
2018-11-05 17:58:46 -0500

    update step_117

---
+++ Step_117/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_117
+{
+    //declare a class as static
+    public static class Class1
+    {
+        //void method that outputs an integer
+        //method divides data passed to it by 2
+        //ref num 1 is output parameter
+        public static void DivideBy2(ref int num1)
+        {
+             num1 = num1 / 2;           
+        }
+
+        //Overload a method
+        public static int DivideBy2(int num1, out int num2)
+        {
+            num2 = num1 / 2;
+            return num2;
+        }
+
+    }
+}
---
+++ Step_117/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to be divided by 2:");           
+            int num1 = Convert.ToInt32(Console.ReadLine());         
+            Class1.DivideBy2(ref num1);
+            Console.WriteLine(num1);           
+            Console.ReadLine();
+        }
+    }
+}
