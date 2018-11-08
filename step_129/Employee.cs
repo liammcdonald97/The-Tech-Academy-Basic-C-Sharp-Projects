3b279928a237d2701624ebba89ae118f5ea840a0
liammcdonald97 <liammcdonald97@gmail.com>
2018-11-07 20:00:01 -0500

    update 129

---
+++ step_129/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_124
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);           
+        }
+
+        public void Quit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator== (Employee a, Employee b)
+        {
+            if(a.Id == b.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public static bool operator!= (Employee a, Employee b)
+        {
+            if(a.Id != b.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
